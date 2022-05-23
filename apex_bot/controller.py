251b37fc2f7b35235655040bfe46d18cc2a1c07d
ktaroabobon <ktaroabobon@gmail.com>
2022-05-23 23:46:24 +0900

    add: .github/workflow

--- apex_bot/controller.py
+++ apex_bot/controller.py
@@ -3,8 +3,8 @@ from pathlib import Path
 
 from dateutil import tz
 
-from als import get_info
-from twitter import tweet
+from apex_bot.als import get_info
+from apex_bot.twitter import tweet
 
 MODE_DICT = {
     "battle_royale": "BR カジュアル",
@@ -18,6 +18,9 @@ MAP_NAME_DICT = {
     "olympus": "オリンパス",
     "world's edge": "ワールズエッジ",
     "storm point": "ストームポイント",
+    "kings canyon": "キングスキャニオン",
+    "party crasher": "パーティークラッシャー",
+    "overflow": "オーバーフロー",
 }
 
 TZ_INFO = "Asia/Tokyo"
@@ -37,11 +40,15 @@ def get_tweet_content(map_info: dict, mode: str = "battle_royale") -> dict:
     target_data = map_info[mode]
     start = target_data['current']['start']
     end = target_data['current']['end']
+    next_start = target_data['next']['start']
+    next_end = target_data['next']['end']
     jst = tz.gettz(TZ_INFO)
 
     info = {
         'start': datetime.datetime.fromtimestamp(start).astimezone(jst),
         'end': datetime.datetime.fromtimestamp(end).astimezone(jst),
+        'next_start': datetime.datetime.fromtimestamp(next_start).astimezone(jst),
+        'next_end': datetime.datetime.fromtimestamp(next_end).astimezone(jst),
         'map': MAP_NAME_DICT[(target_data['current']['map']).lower()],
         'asset': MEDIA_DIR / target_data['current']['asset'].split('/')[-1],
         'next_map': MAP_NAME_DICT[(target_data['next']['map']).lower()],
@@ -55,7 +62,8 @@ def get_tweet_content(map_info: dict, mode: str = "battle_royale") -> dict:
 {info['map']}（{str(info['start'].hour).zfill(2)}:{str(info['start'].minute).zfill(2)}～{str(info['end'].hour).zfill(2)}:{str(info['end'].minute).zfill(2)}）
     
 【次回のマップ】
-{info['next_map']}（{str(info['start'].hour).zfill(2)}:{str(info['start'].minute).zfill(2)}～{str(info['end'].hour).zfill(2)}:{str(info['end'].minute).zfill(2)}）   
+{info['next_map']}（{str(info['next_start'].hour).zfill(2)}:{str(info['next_start'].minute).zfill(2)}～{str(info['next_end'].hour).zfill(2)}:{str(info['next_end'].minute).zfill(2)}） 
+#Apex #Maprotation
     """
 
     return {'text': context, 'media': info['asset']}
--- main.py
+++ main.py
@@ -1,32 +1,8 @@
-import json
-import os
-import pathlib
-import requests
+from apex_bot.controller import bot
 
 
 def main() -> None:
-    api_key = os.environ["ALS_API_KEY"]
-    print(f"ALS API KEY: {api_key}")
-
-    mode = "maprotation"
-    url = f"https://api.mozambiquehe.re/{mode}"
-    params = {
-        "auth": api_key,
-        # "uid": "1014171384068",
-        # "platform": "PC",
-        # "history": 1,
-        # "action": "info",
-        "version": 2,
-    }
-
-    res = requests.get(url, params=params)
-
-    print(res.text)
-
-    path = pathlib.Path().resolve() / f"data.json"
-
-    with path.open('w') as f:
-        json.dump(json.loads(res.text), f, indent=4, ensure_ascii=False)
+    bot()
 
 
 if __name__ == '__main__':
